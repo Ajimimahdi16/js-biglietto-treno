581af0d9dfc3fd3961fb2eac88db5ce5253d4a9a
AJIMI MAHDI <AJIMIMAHDI1@GMAIL.COM>
2025-12-03 21:16:06 +0100

    modifica inserendo il console,log finale con tofixed per i decimali dopo la virgola

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-//numero km da percorrere
+
+// numero km da percorrere
 const kmDaPercorrere = Number(prompt("Inserisci il numero di chilometri da percorrere:"));
-//età del passeggero
+
+// età del passeggero
 const etaPasseggero = Number(prompt("Inserisci la tua età:"));
-//prezzo al km
+
+// prezzo al km
 const prezzoAlKm = 0.21;
-//prezzo totale senza sconti
+
+// prezzo totale senza sconti
 let prezzoTotale = kmDaPercorrere * prezzoAlKm;
-//sconto minorenni
-const scontoMinorenni = 0.20; //20%
-//sconto over 65
-const scontoOver65 = 0.40;  //40%
 
-//calcolo sconto minorenni
- if (etaPasseggero < 18) {
-    prezzoTotale =  prezzoTotale - (prezzoTotale * scontoMinorenni);
-    console.log(prezzoTotale);
+// sconti
+const scontoMinorenni = 0.20; // 20%
+const scontoOver65 = 0.40;    // 40%
 
-
-}
-if (etaPasseggero > 65) {
-    //calcolo sconto over 65
-    prezzoTotale =  prezzoTotale - (prezzoTotale * scontoOver65) ;
-    console.log(prezzoTotale);
-
-
-} 
-  //prezzo per età compresa tra 18 e 65 anni
-else if (etaPasseggero >= 18 && etaPasseggero <= 65)
- {
-     console.log(prezzoTotale);
+if (etaPasseggero < 18) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * scontoMinorenni);
+} else if (etaPasseggero > 65) {
+    prezzoTotale = prezzoTotale - (prezzoTotale * scontoOver65);
 }
 
+// output finale
+console.log("Il prezzo finale è: " + prezzoTotale.toFixed(2) + "€");
